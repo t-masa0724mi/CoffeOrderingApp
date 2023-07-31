6659e726f570514def99b96eea9509f06f20359f
masa950724mi <s100.iizuka.masami@gmail.com>
2023-07-31 15:47:54 +0900

    Initial Commit

---
+++ CoffeOrderingApp/CoffeOrderingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeOrderingAppApp.swift
+//  CoffeOrderingApp
+//
+//  Created by 飯塚政美 on 2023/07/31.
+//
+
+import SwiftUI
+
+@main
+struct CoffeOrderingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
